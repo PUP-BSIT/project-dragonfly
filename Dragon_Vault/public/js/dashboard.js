3a8602f35650312b81d155b4bbc316ead9acd77e
barcelos-kevin <barceloskevinjoseph@gmail.com>
2025-06-06 21:09:43 +0800

    linnk index html correctly

--- Dragon_Vault/public/js/dashboard.js
+++ Dragon_Vault/public/js/dashboard.js
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 alert(
                     "Failed to load account information. Please log in again."
                 );
-                window.location.href = "/Dragon_Vault/public/landing_page.html";
+                window.location.href = "../../index.html";
             }
         })
         .catch((err) => {
@@ -106,7 +106,7 @@ function logout() {
         .then((res) => res.json())
         .then((data) => {
             if (data.success) {
-                window.location.href = "/Dragon_Vault/public/landing_page.html";
+                window.location.href = "../../index.html";
             } else {
                 alert("Logout failed. Try again.");
             }

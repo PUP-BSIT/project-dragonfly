a04b24c24f8d365e48e0b7bebf9b66bd4112c08f
barcelos-kevin <barceloskevinjoseph@gmail.com>
2025-06-27 23:03:38 +0800

    update teller registration password_input

--- Dragon_Vault/private/js/teller_registration.js
+++ Dragon_Vault/private/js/teller_registration.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
         branch: document.getElementById("branch").value,
         email: document.getElementById("email").value,
         username: document.getElementById("username").value,
-        password: document.getElementById("password").value
+        password: document.getElementById("password_input").value
       };
 
       registerTeller(formData)

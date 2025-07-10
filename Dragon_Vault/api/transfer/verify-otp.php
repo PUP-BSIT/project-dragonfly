0bb72b3dfb379fca313e1438aa532368768e39ce
barcelos-kevin <barceloskevinjoseph@gmail.com>
2025-07-10 16:11:38 +0800

    remove space

--- Dragon_Vault/api/transfer/verify-otp.php
+++ Dragon_Vault/api/transfer/verify-otp.php
@@ -106,7 +106,7 @@ try {
             $external_payload = [
                 'transaction_amount' => floatval($transaction['amount']),
                 'source_account_no' => $source_account_no,
-                'source_bank_code' => 'DragonVault',
+                'source_bank_code' => 'Dragon Vault',
                 'recipient_account_no' => $transaction['recipient_account_number']
             ];
 
@@ -134,7 +134,7 @@ try {
             $external_payload = [
                 'transaction_amount' => floatval($transaction['amount']),
                 'source_account_no' => $source_account_no,
-                'source_bank_code' => 'Dragon Vault',
+                'source_bank_code' => 'DragonVault',
                 'recipient_account_no' => $transaction['recipient_account_number']
             ];
 

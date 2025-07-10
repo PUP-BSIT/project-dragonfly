3131f2e469e1d15f9375d108732e850fa3e0edbb
gomez-ashleyhermione <hermionegomez49@gmail.com>
2025-07-10 20:55:49 +0800

    Toggle password and simplify the button effects (#120)

--- Dragon_Vault/public/js/login.js
+++ Dragon_Vault/public/js/login.js
@@ -4,6 +4,8 @@ const API_BASE = location.hostname === "localhost"
 
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.getElementById("login_form");
+    const passwordInput = document.getElementById("login_password");
+    const passwordToggle = document.getElementById("password_toggle");
 
     // Named function to handle login
     function handleLogin(username, password) {
@@ -13,6 +15,28 @@ document.addEventListener("DOMContentLoaded", () => {
             body: JSON.stringify({ username, password }),
         }).then((response) => response.json());
     }
+
+    // Show/hide password toggle icon when user types
+    passwordInput.addEventListener("input", function() {
+        if (passwordInput.value.length > 0) {
+            passwordToggle.style.display = "block";
+        } else {
+            passwordToggle.style.display = "none";
+        }
+    });
+
+    // Toggle password visibility
+    passwordToggle.addEventListener("click", function() {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            passwordToggle.src = "../assets/unhide.png";
+            passwordToggle.alt = "Hide Password";
+        } else {
+            passwordInput.type = "password";
+            passwordToggle.src = "../assets/hide.png";
+            passwordToggle.alt = "Show Password";
+        }
+    });
 
     // Login form submit event
     if (loginForm) {

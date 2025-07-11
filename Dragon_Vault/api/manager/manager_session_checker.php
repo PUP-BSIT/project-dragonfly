8ffa013f88fe76a13c23bfa989d318e48b3569e3
barcelos-kevin <barceloskevinjoseph@gmail.com>
2025-07-11 17:13:06 +0800

    add the header.php for session start

--- Dragon_Vault/api/manager/manager_session_checker.php
+++ Dragon_Vault/api/manager/manager_session_checker.php
@@ -1,6 +1,7 @@
 <?php
-header('Content-Type: application/json');
+require_once __DIR__ . '/../_headers.php';
 echo json_encode([
-    'logged_in' => isset($_SESSION['manager_id'])
+    'logged_in' => isset($_SESSION['manager_id']),
+    'manager_id' => $_SESSION['manager_id'] ?? null,
 ]);
 exit(); 
